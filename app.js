3bdf361338a80a258c1be375e092c114103080ef
Damien White <damienkwhite@gmail.com>
2024-01-27 22:51:22 +0000

    added a few event listeners

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const longNumb = document.querySelector(".longNumber");
 const longNumInput = document.querySelector("#longNumInput");
 
+const nameCard = document.querySelector(".nameCard");
+const nameCardInput = document.querySelector("#nameCardInput");
+
+const month = document.querySelector("#month");
+const year = document.querySelector("#year");
+const monthInput = document.querySelector("#monthInput");
+const yearInput = document.querySelector("#yearInput");
+
+monthInput.addEventListener("input", e => {
+    e.preventDefault();
+    month.innerText = monthInput.value;
+})
+
+yearInput.addEventListener("input", e => {
+    e.preventDefault();
+    year.innerText = yearInput.value;
+})
+
+nameCardInput.addEventListener("input", e => {
+    e.preventDefault();
+    nameCard.innerText = nameCardInput.value;
+})
+
 longNumInput.addEventListener("input", e => {
     e.preventDefault();
     longNumb.innerText = longNumInput.value;
-    if (!Number.isInteger(Number(longNumInput.value))) {
-        console.log("not a number");
+    if(longNumInput.value === Number.isInterger()){
+        console.log("not a number")
     } 
 })
