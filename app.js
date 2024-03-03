c451b34fdb89a0122d4c29e7fd80df5ab8f9ec8c
CtrlAltSudo <damienkwhite@gmail.com>
2024-03-03 12:12:47 +0000

    test

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ const nameCardInput = document.querySelector("#nameCardInput");
 const longNumInput = document.querySelector("#longNumInput");
 const monthInput = document.querySelector("#monthInput");
 const yearInput = document.querySelector("#cvcInput");
+
+/* Testing */
